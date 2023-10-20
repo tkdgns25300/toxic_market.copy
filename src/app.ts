7093e329ab8665f146eeeb3c24a4cda30e7f424a
sanghun lee <tkdgns25300@gmail.com>
2023-10-20 14:01:35 +0900

    feat: set Database Connect

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 import { AppDataSource } from "../ormconfig";
+import path from "path";
+import { DataSource } from "typeorm";
 
 export class App {
 	public app: express.Application;
@@ -13,32 +14,52 @@ export class App {
 		this.setMiddlewares();
 	}
 
+	// Set .env file
+	private setEnv(): void {
+		if (process.env.NODE_ENV === "development") {
+			dotenv.config({
+				path: path.join(__dirname, "../.env.development"),
+			});
+		} else if (process.env.NODE_ENV === "production") {
+			dotenv.config({
+				path: path.join(__dirname, "../.env.production"),
+			});
+		}
+	}
+
 	// Set Database Connect
 	private setDatabase(): void {
+		const AppDataSource = new DataSource({
+			type: "mysql",
+			host: process.env.DB_HOST,
+			port: 3306,
+			username: process.env.DB_USER_NAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			connectTimeout: 3000,
+			entities: ["src/entity/**/*.{js,ts}"],
+		});
+
 		AppDataSource.initialize()
 			.then(() => {
-				console.log("Data Source has been initialized");
+				console.log("Database Connected successfully");
 			})
 			.catch((err) => {
-				console.error("Error during Data Source initialization", err);
+				console.error("Error during Database Connection", err);
 			});
 	}
 
 	// Set Middlewares
 	private setMiddlewares(): void {}
 
-	// Set .env file
-	private setEnv(): void {
-		if (process.env.NODE_ENV === "development") {
-			dotenv.config({
-				path: "../.env.development",
+	// Start Express Server
+	public async createExpressServer(): Promise<void> {
+		try {
+			this.app.listen(4000, () => {
+				console.log("Server Start at 4000 port");
 			});
-		} else if (process.env.NODE_ENV === "production") {
-			dotenv.config({
-				path: "../.env.production",
-			});
+		} catch (error) {
+			console.log(error);
 		}
 	}
 }
-
-// setEnv 함수 먼저 작성해야 DB연결 시 환경변수 사용 가능.
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { App } from "./app";
+
+try {
+  const app = new App();
+  app.createExpressServer();
+} catch (err) {
+  console.log(err);
+}
