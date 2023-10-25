e2cfffc9ef8c4190173995656e362fbfdd82fd2d
sanghun lee <tkdgns25300@gmail.com>
2023-10-25 18:51:28 +0900

    feat: Token auth

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User } from "../entity/user";
+dotenv.config();
+
+// Extract AccessToken
+const extractAccessToken = (req: Request) => {
+	if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+		return req.headers.authorization.split(" ")[1];
+	} else {
+		throw new Error("JWT token does not exist");
+	}
+};
+
+// Check Access Token
+export const checkAccessToken = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const token = extractAccessToken(req);
+		const jwtPayload = jwt.verify(token, process.env.JWT_TOKEN_KEY);
+		res.locals.jwtPayload = jwtPayload;
+	} catch (error) {
+		return res.status(401).send({ message: "Invalid token value" });
+	}
+
+	next();
+};
+
+// Check Admin Access Token
+export const checkAdminAccessToken = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const token = extractAccessToken(req);
+		const jwtPayload = jwt.verify(token, process.env.JWT_TOKEN_KEY);
+		if (!jwtPayload.admin) {
+			throw new Error();
+		}
+		res.locals.jwtPayload = jwtPayload;
+	} catch (error) {
+		return res.status(401).send({ message: "Not an administrator." });
+	}
+
+	next();
+};
+
+// Make a Access Token
+export const generateAccessToken = (user: User) => {
+	return jwt.sign(
+		{
+			aud: user.public_address,
+			admin: user.is_admin === "O",
+		},
+		process.env.JWT_TOKEN_KEY,
+		{ expiredIn: "3h" },
+	);
+};
